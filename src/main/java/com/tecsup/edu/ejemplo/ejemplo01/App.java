61c6c3ee9839515a666e9f2d729729e7e55fd7c2
jhonvillanueva44 <jhon.villanueva.l@tecsup.edu.pe>
2024-09-11 02:15:02 -0500

    Subiendo mi proyecto Maven

---
+++ src/main/java/com/tecsup/edu/ejemplo/ejemplo01/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.edu.ejemplo.ejemplo01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
